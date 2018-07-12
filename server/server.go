1a2f558e370e210a183cc17ff21b5ddea9bc5d60
mnawaz <mnawaz@thoughtworks.com>
2018-07-12 20:19:45 +0530

    Task Handler to handle POST requests only

---
+++ server/server.go
@@ -0,0 +1,20 @@
+package server
+
+import (
+	"log"
+	"net/http"
+)
+
+func LaunchServerOn(port string) {
+
+	http.HandleFunc("/task", HandleTask)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func HandleTask(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != "POST" {
+		http.Error(w, "Unsupported Method", http.StatusMethodNotAllowed)
+		return
+	}
+}
