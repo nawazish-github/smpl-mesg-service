283ef384acd8638f98ae696af756f47a048e86b4
mnawaz <mnawaz@thoughtworks.com>
2018-07-14 21:35:55 +0530

    Use Requester to make outbound calls and do other setups

--- server/server.go
+++ server/server.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
 //LaunchServerOn Listens for incoming http connections on given port
@@ -16,6 +18,8 @@ func LaunchServerOn(port string) {
 
 //HandleTask handles POST request, understood as tasks, with well-formed payload
 func HandleTask(w http.ResponseWriter, r *http.Request) {
+
+	reqTime := initialize()
 
 	if r.Method != "POST" {
 		http.Error(w, "Unsupported Method", http.StatusMethodNotAllowed)
@@ -29,7 +33,7 @@ func HandleTask(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var es task
+	var es Task
 	err = json.Unmarshal(reqBody, &es)
 
 	if err != nil {
@@ -42,4 +46,45 @@ func HandleTask(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	requester := Requester{
+		client: es.Execute,
+	}
+
+	resp, err := requester.Request()
+
+	if err != nil {
+		http.Error(w, "Could not make POST request to upstream service", http.StatusInternalServerError)
+		return
+	}
+
+	event := generateEvent(reqTime, rand.Int(), resp)
+	mEvent, err := marshalEvent(event)
+
+	if err != nil {
+		http.Error(w, "Internal Server Error: could not unmarshal POST body", http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(mEvent)
+
+}
+
+func generateEvent(date time.Time, id int, body []byte) *Event {
+	return &Event{
+		OnRequest: OnRequest{
+			Time: date,
+			Id:   id,
+			Body: body,
+		},
+	}
+}
+
+func marshalEvent(e *Event) ([]byte, error) {
+	return json.Marshal(e)
+}
+
+func initialize() time.Time {
+	reqTime := time.Now()
+	rand.Seed(time.Now().UnixNano())
+	return reqTime
 }
