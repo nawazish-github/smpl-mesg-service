c118d22f0dfaf36645337989004385f351d675e3
mnawaz <mnawaz@thoughtworks.com>
2018-07-14 21:31:02 +0530

    Create an Stub and a Requester type

--- server/server_models.go
+++ server/server_models.go
@@ -1,10 +1,31 @@
 package server
 
-type task struct {
-	Execute execute `json:execute`
+import "time"
+
+type Task struct {
+	Execute Execute `json:execute`
 }
 
-type execute struct {
+type Execute struct {
 	URL  string `json:url`
 	Body string `json:body`
+}
+
+type Event struct {
+	OnRequest OnRequest `json:onRequest`
+}
+
+type OnRequest struct {
+	Time time.Time `json:time`
+	Id   int       `json:id`
+	Body []byte    `json:body`
+}
+
+//StubExecute helps Unit Test Client.POST method
+type StubExecute struct{}
+
+//Requester holds an instance of Client implementation
+//to make POST calls
+type Requester struct {
+	client Client
 }
