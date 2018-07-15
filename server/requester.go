47d521c70dbd518459306a600d46616c3cf255a3
mnawaz <mnawaz@thoughtworks.com>
2018-07-15 18:11:09 +0530

    Provide new Requester implementation to make batch requests

--- server/requester.go
+++ server/requester.go
@@ -1,5 +1,9 @@
 package server
 
+import (
+	"encoding/json"
+)
+
 //IPOSTRequest interface is a generic interface to make all sorts of
 //out bound calls from this server. An implementation of this
 //interface - a "Requester" - in conjunction with a "Client",
@@ -12,4 +16,26 @@ type IPOSTRequest interface {
 func (r Requester) Request() ([]byte, int, error) {
 	b, c, e := r.client.POST()
 	return b, c, e
+}
+
+//Request is an implementation of the IPOSTRequest contract.
+//Implemented by BatchRequester. The fires concurrent POST
+//requests using the client.
+func (br *BatchRequester) Request() ([]byte, int, error) {
+	successMap := make(map[string]Success)
+	for _, execute := range br.batchExecute.BatchRequests {
+		go func() {
+			respBytes, statusCode, _ := execute.POST()
+			successMap[execute.URL] = Success{
+				Body:       respBytes,
+				StatusCode: statusCode,
+			}
+		}()
+	}
+
+	mapBytes, err := json.Marshal(successMap)
+	if err != nil {
+		return nil, 500, err
+	}
+	return mapBytes, 200, nil
 }
