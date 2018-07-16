6047cd8cd7b057dca63674560d11500442e836fa
mnawaz <mnawaz@thoughtworks.com>
2018-07-16 14:31:46 +0530

    Add wait group so that POST Handler returns only after executing all child requests

--- server/requester.go
+++ server/requester.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"encoding/json"
+	"sync"
 )
 
 //IPOSTRequest interface is a generic interface to make all sorts of
@@ -23,8 +24,12 @@ func (r Requester) Request() ([]byte, int, error) {
 //requests using the client.
 func (br *BatchRequester) Request() ([]byte, int, error) {
 	successMap := make(map[string]Success)
+	var wg sync.WaitGroup
+	wg.Add(len(br.batchExecute.BatchRequests))
+
 	for _, execute := range br.batchExecute.BatchRequests {
 		go func() {
+			defer wg.Done()
 			respBytes, statusCode, _ := execute.POST()
 			successMap[execute.URL] = Success{
 				Body:       respBytes,
@@ -33,6 +38,7 @@ func (br *BatchRequester) Request() ([]byte, int, error) {
 		}()
 	}
 
+	wg.Wait()
 	mapBytes, err := json.Marshal(successMap)
 	if err != nil {
 		return nil, 500, err
